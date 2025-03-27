d7db0d8345e37f9585e13d7e810dea3142320a7b
gulshanbharti07 <gulshan2819@gmail.com>
2025-03-27 20:02:32 +0530

    Updated db.js to use environment variables

--- db.js
+++ db.js
@@ -1,5 +1,39 @@
 // const mysql = require('mysql2');
 
+
+// // MySQL connection details
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',         // Change this if you're using a different user
+//     password: 'NewPassword@123',  // Enter your MySQL password here
+//     database: 'mysite'    // Name of your MySQL database
+// });
+
+// // db.connect((err) => {
+// //     if (err) {
+// //         console.error('❌ MySQL Connection Failed:', err.stack);
+// //     } else {
+// //         console.log('✅ Connected to MySQL Database');
+// //     }
+// // });
+
+// // module.exports = db;
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//     host: 'mysql.railway.internal',
+//     user: 'root',
+//     password: 'lBwLLkfLOQMMokxhdDSIPQTfMzcviGIY',
+//     database: 'railway',
+//     port: 3306
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error("❌ MySQL Connection Failed:", err);
+//         return;
+
+
 // // MySQL connection details
 // const db = mysql.createConnection({
 //     host: 'localhost',
@@ -17,20 +51,23 @@
 // });
 
 // module.exports = db;
-const mysql = require('mysql2');
+
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'mysql.railway.internal',
+    host: 'nozomi.proxy.rlwy.net',
     user: 'root',
     password: 'lBwLLkfLOQMMokxhdDSIPQTfMzcviGIY',
     database: 'railway',
-    port: 3306
+    port: '19266'
 });
 
-connection.connect((err) => {
+connection.connect(err => {
     if (err) {
-        console.error("❌ MySQL Connection Failed:", err);
-        return;
+        console.error('❌ MySQL Connection Failed:', err);
+    } else {
+        console.log('✅ MySQL Connected Successfully!');
+
     }
     console.log("✅ MySQL Connected Successfully!");
 });
